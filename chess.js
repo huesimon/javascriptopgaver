cca8c05e811b87f9b5ce5bbe869089f8e8c4440c
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-02-20 10:59:37 +0100

    loop # fizzbuzzchess

---
+++ chess.js
@@ -0,0 +1,13 @@
+var width = 8;
+var height = 8;
+var string = "";
+
+for (i = 1; i <= width; i++) {
+    string = string + "#";
+}
+//row
+
+
+for(i = 1; i <=height; i++){
+    console.log(string);
+}
