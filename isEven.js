c29ad0bf5b4ad263c77a6c8fe8ff55773343cbd6
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-02-20 11:18:36 +0100

    countBs

--- isEven.js
+++ isEven.js
@@ -1,14 +1,35 @@
-for (i = 0; i <100; i++){
-    isEven(i);
+console.log(min(0,10));
+
+function min(x,y) {
+    if(x > y){
+        return x;
+    }
+    else if(x < y){
+        return y;
+    }
 }
+
+
+// for (i = 0; i <100; i++){
+//     console.log(isEven(i))
+// }
 
 
 function isEven(number) {
     if(number % 2 == 0){
-        console.log(number + "Is Even");
         return true;
     }
     else {
-        console.log(number + "Is not Even")
+        return false;
     }
+}
+
+countBs("BBCXDbbbB");
+function countBs(string) {
+    let counter = 0;
+    for (i = 0; i <= string.length; i++)
+        if (string.charAt(i) == "B"){
+        counter++;
+        }
+    console.log(counter)
 }
